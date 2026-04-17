72c32fedc6f7a13dadaee4d0fd92303141e0203d
erebor-519 <22307140030@m.fudan.edu.cn>
2026-04-18 01:00:11 +0800

    Update TravelPlan.vue

--- aitravel/src/pages/TravelPlan.vue
+++ aitravel/src/pages/TravelPlan.vue
@@ -21,7 +21,7 @@ const leftPanelCollapsed = ref(false)
 const amapService = AMapService
 
 const client = new OpenAI({
-  baseURL: '/api',
+  baseURL: window.location.origin + '/api',
   apiKey: 'b644c04f33fd3a89ed601ec9cdadfddb:MDM3YTllYWVjZTAwMjY4MTM4ZTlhM2Vm',
   defaultHeaders: { 'X-Failover-Enabled': 'true' },
   dangerouslyAllowBrowser: true
